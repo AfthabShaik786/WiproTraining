dfe3f41e830e514e4b079f4f2860da36a5818ceb
Shaik Mohammad Afthab <sh20695879@wipro.com>
2026-04-24 14:39:03 +0530

    Pushing new files

---
+++ count.java
@@ -0,0 +1,9 @@
+package com;
+
+public class count {
+	public static void main(String[] args) {
+		String str = "Shaik Mohammad Afthab";
+		int count = str.length();
+		System.out.println("The no. of characters in string are : "+count);
+	}
+}
